ce1da8bf44aec6db51dd8c107f3d27cdbb255f65
howard <howard.huang@itech99.com>
2025-12-02 14:58:03 +0800

    feat: 添加通用状态

--- rexDatabase/seo.go
+++ rexDatabase/seo.go
@@ -1,9 +1,16 @@
 package rexDatabase
 
-type Seo struct {
+type SeoBase struct {
 	// note: og设置
 	OgTitle       string `json:"og_title"`
 	OgDescription string `json:"og_description"`
 	OgImage       string `json:"og_image"`
 	OgUrl         string `json:"og_url"`
+}
+
+type SeoExtra struct {
+	SeoBase
+	Debug       bool   `gorm:"column:debug;comment:debug;type: boolean;default:false" json:"debug"`
+	Card        string `gorm:"column:card;comment:分享卡片;type: varchar(255);" json:"card"`                    // 分享卡片
+	CreatorSite string `gorm:"column:creator_site;comment:示例@xxxx;type: varchar(255);" json:"creator_site"` // 创建者站点
 }
