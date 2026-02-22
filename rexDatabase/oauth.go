e07e7efbaf99b38192ac9b13002237195fa7fb99
howard <howard.huang@itech99.com>
2026-02-22 18:24:03 +0800

    feat: 添加通用的比较函数

--- rexDatabase/oauth.go
+++ rexDatabase/oauth.go
@@ -1,8 +1,15 @@
 package rexDatabase
 
+//EXISTING_USER       // 老用户正常登录
+//AUTO_REGISTER       // 自动注册
+//FIRST_LOGIN         // 首次登录
+//ACCOUNT_LINKED      // 第三方账号绑定
+//INVITED_REGISTER    // 邀请注册
+
 const (
-	OauthMetaLoginTypeNormaLogin      = "EXISTING_USER"
+	OauthMetaLoginTypeExistingUser    = "EXISTING_USER"
 	OauthMetaLoginTypeAutoRegister    = "AUTO_REGISTER"
+	OauthMetaLoginTypeFirstLogin      = "FIRST_LOGIN"
 	OauthMetaLoginTypeAccountLinked   = "ACCOUNT_LINKED"
 	OauthMetaLoginTypeInvitedRegister = "INVITED_REGISTER"
 )
