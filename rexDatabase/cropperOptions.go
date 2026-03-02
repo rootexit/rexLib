0fa04e2f3665f1c9c90f9376261ee4852334ac7c
howard <howard.huang@itech99.com>
2026-03-02 14:53:24 +0800

    feat: 添加CropperOptions

--- rexDatabase/cropperOptions.go
+++ rexDatabase/cropperOptions.go
@@ -76,34 +76,34 @@ type CropperOptions struct {
 	//AspectRatioHeight float64 `gorm:"column:aspect_ratio_height;comment:纵横比高度;type:float;default:1" json:"aspect_ratio_height"`
 	//Data                     []byte                 `gorm:"-" json:"-"`
 	//Preview                  []byte                 `gorm:"-" json:"-"`
-	Responsive               bool  `gorm:"column:responsive;comment:响应式设计;type:boolean;default:true" json:"responsive"`
-	Restore                  bool  `gorm:"column:restore;comment:恢复,调整窗口大小后，恢复被裁剪的区域。;type:boolean;default:true" json:"restore"`
-	CheckCrossOrigin         bool  `gorm:"column:check_cross_origin;comment:检查跨域;type:boolean;default:true" json:"check_cross_origin"`
-	CheckOrientation         bool  `gorm:"column:check_orientation;comment:检查方向;type:boolean;default:true" json:"check_orientation"`
-	Modal                    bool  `gorm:"column:modal;comment:模态,在图像上方、裁剪框下方显示黑色模态框。;type:boolean;default:true" json:"modal"`
-	Guides                   bool  `gorm:"column:guides;comment:指南;type:boolean;default:true" json:"guides"`
-	Center                   bool  `gorm:"column:center;comment:中心;type:boolean;default:true" json:"center"`
-	Highlight                bool  `gorm:"column:highlight;comment:强调，在裁剪框上方显示白色模态框（突出显示裁剪框）;type:boolean;default:true" json:"highlight"`
-	Background               bool  `gorm:"column:background;comment:背景,显示容器的网格背景;type:boolean;default:true" json:"background"`
-	AutoCrop                 bool  `gorm:"column:auto_crop;comment:自动裁剪;type:boolean;default:true" json:"auto_crop"`
-	AutoCropArea             bool  `gorm:"column:auto_crop_area;comment:自动裁剪区域,0-1;type:boolean;default:true" json:"auto_crop_area"`
-	Movable                  bool  `gorm:"column:movable;comment:活动,启用此功能可移动图像;type:boolean;default:true" json:"movable"`
-	Rotatable                bool  `gorm:"column:rotatable;comment:可旋转;type:boolean;default:true" json:"rotatable"`
-	Scalable                 bool  `gorm:"column:scalable;comment:可扩展;type:boolean;default:true" json:"scalable"`
-	Zoomable                 bool  `gorm:"column:zoomable;comment:可缩放;type:boolean;default:true" json:"zoomable"`
-	ZoomOnTouch              bool  `gorm:"column:zoom_on_touch;comment:触摸缩放;type:boolean;default:true" json:"zoom_on_touch"`
-	ZoomOnWheel              bool  `gorm:"column:zoom_on_wheel;comment:车轮上的缩放;type:boolean;default:true" json:"zoom_on_wheel"`
-	WheelZoomRatio           bool  `gorm:"column:wheel_zoom_ratio;comment:轮毂缩放比,定义使用鼠标滚轮缩放图像时的缩放比例;type:boolean;default:true" json:"wheel_zoom_ratio"`
-	CropBoxMovable           bool  `gorm:"column:crop_box_movable;comment:可移动的裁剪盒,启用此功能后，即可通过拖动来移动裁剪框;type:boolean;default:true" json:"crop_box_movable"`
-	CropBoxResizable         bool  `gorm:"column:crop_box_resizable;comment:允许通过拖动来调整裁剪框的大小;type:boolean;default:true" json:"crop_box_resizable"`
-	ToggleDragModeOnDblclick bool  `gorm:"column:toggle_drag_mode_on_dblclick;comment:双击切换拖动模式;type:boolean;default:true" json:"toggle_drag_mode_on_dblclick"`
-	MinContainerWidth        int64 `gorm:"column:min_container_width;comment:最小容器宽度;type: bigint;default:200" json:"min_container_width"`
-	MinContainerHeight       int64 `gorm:"column:min_container_height;comment:最小容器高度;type: bigint;default:100" json:"min_container_height"`
-	MinCanvasWidth           int64 `gorm:"column:min_canvas_width;comment:最小canvas宽度;type: bigint;default:0" json:"min_canvas_width"`
-	MinCanvasHeight          int64 `gorm:"column:min_canvas_height;comment:最小canvas高度;type: bigint;default:0" json:"min_canvas_height"`
-	MinCropBoxWidth          int64 `gorm:"column:min_crop_box_width;comment:最小裁切框宽度;type: bigint;default:0" json:"min_crop_box_width"`
-	MinCropBoxHeight         int64 `gorm:"column:min_crop_box_height;comment:最小裁切框高度;type: bigint;default:0" json:"min_crop_box_height"`
-	CropperExtraOptions
+	Responsive               bool                `gorm:"column:responsive;comment:响应式设计;type:boolean;default:true" json:"responsive"`
+	Restore                  bool                `gorm:"column:restore;comment:恢复,调整窗口大小后，恢复被裁剪的区域。;type:boolean;default:true" json:"restore"`
+	CheckCrossOrigin         bool                `gorm:"column:check_cross_origin;comment:检查跨域;type:boolean;default:true" json:"check_cross_origin"`
+	CheckOrientation         bool                `gorm:"column:check_orientation;comment:检查方向;type:boolean;default:true" json:"check_orientation"`
+	Modal                    bool                `gorm:"column:modal;comment:模态,在图像上方、裁剪框下方显示黑色模态框。;type:boolean;default:true" json:"modal"`
+	Guides                   bool                `gorm:"column:guides;comment:指南;type:boolean;default:true" json:"guides"`
+	Center                   bool                `gorm:"column:center;comment:中心;type:boolean;default:true" json:"center"`
+	Highlight                bool                `gorm:"column:highlight;comment:强调，在裁剪框上方显示白色模态框（突出显示裁剪框）;type:boolean;default:true" json:"highlight"`
+	Background               bool                `gorm:"column:background;comment:背景,显示容器的网格背景;type:boolean;default:true" json:"background"`
+	AutoCrop                 bool                `gorm:"column:auto_crop;comment:自动裁剪;type:boolean;default:true" json:"auto_crop"`
+	AutoCropArea             bool                `gorm:"column:auto_crop_area;comment:自动裁剪区域,0-1;type:boolean;default:true" json:"auto_crop_area"`
+	Movable                  bool                `gorm:"column:movable;comment:活动,启用此功能可移动图像;type:boolean;default:true" json:"movable"`
+	Rotatable                bool                `gorm:"column:rotatable;comment:可旋转;type:boolean;default:true" json:"rotatable"`
+	Scalable                 bool                `gorm:"column:scalable;comment:可扩展;type:boolean;default:true" json:"scalable"`
+	Zoomable                 bool                `gorm:"column:zoomable;comment:可缩放;type:boolean;default:true" json:"zoomable"`
+	ZoomOnTouch              bool                `gorm:"column:zoom_on_touch;comment:触摸缩放;type:boolean;default:true" json:"zoom_on_touch"`
+	ZoomOnWheel              bool                `gorm:"column:zoom_on_wheel;comment:车轮上的缩放;type:boolean;default:true" json:"zoom_on_wheel"`
+	WheelZoomRatio           bool                `gorm:"column:wheel_zoom_ratio;comment:轮毂缩放比,定义使用鼠标滚轮缩放图像时的缩放比例;type:boolean;default:true" json:"wheel_zoom_ratio"`
+	CropBoxMovable           bool                `gorm:"column:crop_box_movable;comment:可移动的裁剪盒,启用此功能后，即可通过拖动来移动裁剪框;type:boolean;default:true" json:"crop_box_movable"`
+	CropBoxResizable         bool                `gorm:"column:crop_box_resizable;comment:允许通过拖动来调整裁剪框的大小;type:boolean;default:true" json:"crop_box_resizable"`
+	ToggleDragModeOnDblclick bool                `gorm:"column:toggle_drag_mode_on_dblclick;comment:双击切换拖动模式;type:boolean;default:true" json:"toggle_drag_mode_on_dblclick"`
+	MinContainerWidth        int64               `gorm:"column:min_container_width;comment:最小容器宽度;type: bigint;default:200" json:"min_container_width"`
+	MinContainerHeight       int64               `gorm:"column:min_container_height;comment:最小容器高度;type: bigint;default:100" json:"min_container_height"`
+	MinCanvasWidth           int64               `gorm:"column:min_canvas_width;comment:最小canvas宽度;type: bigint;default:0" json:"min_canvas_width"`
+	MinCanvasHeight          int64               `gorm:"column:min_canvas_height;comment:最小canvas高度;type: bigint;default:0" json:"min_canvas_height"`
+	MinCropBoxWidth          int64               `gorm:"column:min_crop_box_width;comment:最小裁切框宽度;type: bigint;default:0" json:"min_crop_box_width"`
+	MinCropBoxHeight         int64               `gorm:"column:min_crop_box_height;comment:最小裁切框高度;type: bigint;default:0" json:"min_crop_box_height"`
+	CropperExtraOptions      CropperExtraOptions `gorm:"embedded" json:"extra"`
 }
 
 type CropperOptionsAspectRatio struct {
