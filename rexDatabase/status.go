fbb62da787fb4b0fb5c619a869803e66d3cbab06
howard <howard.huang@itech99.com>
2026-03-04 13:37:55 +0800

    feat: 添加file和folder

--- rexDatabase/status.go
+++ rexDatabase/status.go
@@ -25,14 +25,17 @@ const (
 	VerifiedStatusVerified
 )
 
-type CommonContentStatus int8
+type CommonContentStatus int32
+
+// note: 文章状态, 1->预草稿,2->草稿,3->提审,4->驳回,5->下架,6->上架（仅列表）,7->仅置顶,8->推荐(置顶+列表)
 
 const (
-	ContentStatusDraft CommonContentStatus = iota + 1
+	ContentStatusPreDraft CommonContentStatus = iota + 1
+	ContentStatusDraft
 	ContentStatusSubmitted
 	ContentStatusRejected
 	ContentStatusUnPublished
 	ContentStatusPublished
-	ContentStatusPinnedOnly
-	ContentStatusPinned
+	ContentStatusTopOnly
+	ContentStatusRecommend
 )
