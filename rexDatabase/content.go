cc3132f893714a3a817f00ec689cd5b74c1c36f8
howard <howard.huang@itech99.com>
2026-04-10 14:01:11 +0800

    feat: 更新一些固定的常量

--- rexDatabase/content.go
+++ rexDatabase/content.go
@@ -2,7 +2,7 @@ package rexDatabase
 
 type ContentType string
 
-// note: 内容类型，1->H5,2->外部链接,3->小程序,4->视频文章,5->PDF文件,6->快讯;
+// note: 内容类型，1->H5,2->外部链接,3->小程序,4->视频文章,5->PDF文件,6->快讯,7->海报;
 
 const (
 	ContentTypeH5      ContentType = "h5"
@@ -11,6 +11,7 @@ const (
 	ContentTypeVideo   ContentType = "video"
 	ContentTypePdf     ContentType = "pdf"
 	ContentTypeNews    ContentType = "express"
+	ContentTypePoster  ContentType = "poster"
 )
 
 type VisibleType string
